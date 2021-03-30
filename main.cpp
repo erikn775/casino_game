73b492991ec7731d2dc393cc4f627c4a77d8a11f
erikn775 <erikn775@gmail.com>
2021-03-30 01:50:21 -0600

    added while loop for readiness question

--- main.cpp
+++ main.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
 #include <stdlib.h>   
 #include <time.h>
+#include <unistd.h>
 
 using namespace std;
 
 int main(){
-    string name;
+    string name, ready;
+    
     int num1, num2, num3;
+
+    cout << "!!!!!!!!!!!Welcome!!!!!!!!!!! \n";
+    cout << "Hello! What is your name: ";
+    cin >> name;
+    cout << name << " Are you ready to play?(Y/n) \n";
+    cin >> ready;
 
     srand (time(NULL));
     num1 = rand() % 10 + 1;
     num2 = rand() % 10 + 1;
-    num2 = rand() % 10 + 1;
+    num3 = rand() % 10 + 1;
 
-    cout << "!!!!!!!!!!!Welcome!!!!!!!!!!!" << endl;
-    cout << "Hello! What is your name: ";
-    cin >> name;
+    while (ready != "Y"){
+        cout << "Booooooooo =/ \n";
+        sleep(5);
+        cout << "You ready now?(Y/n) \n";
+        cin >> ready;
+    }
     
+    cout << num1 << " " << num2 << " " << num3 << endl;
 
     return 0;
 }
