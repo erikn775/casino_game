d2b071b45be9721a769189d961e2995a36070e64
erikn775 <erikn775@gmail.com>
2021-04-16 20:27:59 -0600

    added 4th slot machine number

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(){
     string name, ready;
     
-    int num1, num2, num3;
+    int num1, num2, num3, num4;
 
     cout << "!!!!!!!!!!!Welcome!!!!!!!!!!! \n";
     cout << "Hello! What is your name: ";
@@ -20,6 +20,7 @@ int main(){
     num1 = rand() % 10 + 1;
     num2 = rand() % 10 + 1;
     num3 = rand() % 10 + 1;
+    num4 = rand() % 10 + 1;
 
     while (ready != "Y"){
         cout << "Booooooooo =/ \n";
@@ -29,7 +30,13 @@ int main(){
     }
     
     sleep(2);
-    cout << "///////// " << num1 << " " << num2 << " " << num3 << " /////////\n";
+    cout << "///////// " << num1 << " " << num2 << " " << num3 << " " << num4 << " /////////\n";
+
+    int jackpot = rand();
+
+    //if(num1 == 7 && num2 == 7 && num3 == 7 && num4 == 7){
+        cout << "JACKPOT You Win $" << jackpot << endl;
+    //}
 
     return 0;
 }
